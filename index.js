76ca27a3691bfabb9212157b52b55e6ba4d49811
i8lex <cybeerid@gmail.com>
2022-10-20 19:11:57 +0300

    almost all logic

--- index.js
+++ index.js
@@ -1,199 +1,219 @@
-// import {renderName} from "./render.js";
-// import {renderSection} from "./render.js";
 
-
-const data = [
-    {
-        name:`Jane Cooper`,
-        company:`Microsoft`,
-        phone:`(225) 555-0118`,
-        email:`jane@microsoft.com`,
-        country:`United States`,
-        status: true,
-    },
-    {
-        name:`Floyd Miles`,
-        company:`Yahoo`,
-        phone:`(205) 555-0100`,
-        email:`floyd@yahoo.com`,
-        country:`Kiribati`,
-        status:false,
-    },
-    {
-        name:`Ronald Richards`,
-        company:`Adobe`,
-        phone:`(302) 555-0107`,
-        email:`ronald@adobe.com`,
-        country:`Israel`,
-        status:false,
-    },
-    {
-        name:`Marvin McKinney`,
-        company:`Tesla`,
-        phone:`(252) 555-0126`,
-        email:`marvin@tesla.com`,
-        country:`Iran`,
-        status:true,
-    },
-    {
-        name:`Jerome Bell`,
-        company:`Google`,
-        phone:`(629) 555-0129`,
-        email:`jerome@google.com`,
-        country:`Réunion`,
-        status:true,
-    },
-    {
-        name:`Kathryn Murphy`,
-        company:`Microsoft`,
-        phone:`(406) 555-0120`,
-        email:`kathryn@microsoft.com`,
-        country:`Curaçao`,
-        status:true,
-    },
-    {
-        name:`Jacob Jones`,
-        company:`Yahoo`,
-        phone:`(208) 555-0112`,
-        email:`jacob@yahoo.com`,
-        country:`Brazil`,
-        status:true,
-    },
-    {
-        name:`Kristin Watson`,
-        company:`Facebook`,
-        phone:`(704) 555-0127`,
-        email:`kristin@facebook.com`,
-        country:`Åland Islands`,
-        status:false,
-    },
-]
+import {data} from "./list.js";
 
 const body = document.querySelector (`body`);
-const section = document.createElement(`section`);
-section.className = `section`;
-const container = document.createElement(`div`);
-container.className = `container`;
-const sectionWrapper = document.createElement(`div`);
-sectionWrapper.className = `section__columns`;
-const nameTable = document.createElement(`ul`);
-nameTable.className = `section__texts`;
-const companyTable = document.createElement(`ul`);
-companyTable.className = `section__texts`;
-const phoneTable = document.createElement(`ul`);
-phoneTable.className = `section__texts`;
-const emailTable = document.createElement(`ul`);
-emailTable.className = `section__texts`;
-const countryTable = document.createElement(`ul`);
-countryTable.className = `section__texts`;
-const statusTable = document.createElement(`ul`);
-statusTable.className = `section__texts`;
+body.innerHTML = `
+<header class="header">
+    <div class="header__wrapper">
+        <div class="header__container">
+            <h1 class="header__title">All Customers</h1>
+            <p class="header__link">Active Members</p>
+        </div>
+            <form action="#" class="header__form">
+                    <button type="button"  class="header__search__label__icon">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" stroke="#7E7E7E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M21 21L16.65 16.65" stroke="#7E7E7E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </button>
 
-const nameTitle = document.createElement(`li`);
-nameTitle.className = `section__titles`;
-nameTitle.innerText = `Customer Name`
-const companyTitle = document.createElement(`li`);
-companyTitle.className = `section__titles`;
-companyTitle.innerText = `Company`;
-const phoneTitle = document.createElement(`li`);
-phoneTitle.className = `section__titles`;
-phoneTitle.innerText = `Phone Number`;
-const emailTitle = document.createElement(`li`);
-emailTitle.className = `section__titles`;
-emailTitle.innerText = `Email`
-const countryTitle = document.createElement(`li`);
-countryTitle.className = `section__titles`;
-countryTitle.innerText = `Country`;
-const statusTitle = document.createElement(`li`);
-statusTitle.innerText = `Status`;
-statusTitle.className = `section__titles`;
+                <label for="search" class="header__search__label">
+                    <input type="text" name = "search" class="header__search" id="search" placeholder="Search">
+                </label>
+            </form>
+    </div>
+</header>
+
+<section class="section">
+    <div class="container">
+        <div class="section__wrapper">
+            
+        </div>
+    </div>
+</section>
+
+<footer class="footer">
+    <div class="footer__wrapper">
+        <p class="footer__text"></p>
+    </div>
+</footer>
+`
+
+const sectionWrapper = document.querySelector(`.section__wrapper`);
+
+function renderSection() {
+    let currentPage = 1;
+    let rows = 8;
+    let newData = [];
+
+    const form = document.querySelector(`form`);
+    form.addEventListener(`keyup`, event => {
+        cleanData();
+        cleanPaginator();
+        event.preventDefault();
+
+        const value = document.querySelector(`input`).value;
+            data.filter((item) => {
+                if(
+                    item.name.toLowerCase().includes(value.toLowerCase()) ||
+                    item.company.toLowerCase().includes(value.toLowerCase()) ||
+                    item.phone.toLowerCase().includes(value.toLowerCase()) ||
+                    item.email.toLowerCase().includes(value.toLowerCase()) ||
+                    item.country.toLowerCase().includes(value.toLowerCase())) {
+                    return newData.push(item);
+                }
+            })
+            renderData(newData, rows, currentPage);
+            displayPagination(newData, rows);
+            newData = [];
+    });
 
 
 
-export function renderSection() {
-    body.append(section);
-    section.append(container);
-    container.append(sectionWrapper);
-    sectionWrapper.append(nameTable);
-    nameTable.after(companyTable);
-    nameTable.append(nameTitle);
-    companyTable.after(phoneTable);
-    companyTable.append(companyTitle);
-    phoneTable.after(emailTable);
-    phoneTable.append(phoneTitle);
-    emailTable.after(countryTable);
-    emailTable.append(emailTitle);
-    countryTable.after(statusTable);
-    countryTable.append(countryTitle);
-    statusTable.append(statusTitle);
-    data.forEach(( {
-                       name,
-                       company,
-                       phone,
-                       email,
-                       country,
-                       status}) =>
-        renderName(
-            name,
-            company,
-            phone,
-            email,
-            country,
-            status)
-    );
-}
+    const active = document.querySelector(`.header__link`);
+    active.addEventListener(`click`, () => {
+        cleanData();
+        cleanPaginator();
 
-export function renderName(
-    userName,
-    userCompany,
-    userPhone,
-    userEmail,
-    userCoutry,
-    userStatus) {
+        data.filter((item) => {
+            if(item.status === true) {
+                return newData.push(item);
+            }
+        })
+        renderData(newData, rows, currentPage);
+        displayPagination(newData, rows);
+        newData = [];
+    });
 
-    const name = document.createElement(`li`);
-    name.className = `section__texts__text`;
-    name.innerText = userName;
-    nameTable.append(name);
 
-    const company = document.createElement(`li`);
-    company.className = `section__texts__text`;
-    company.innerText = userCompany;
-    companyTable.append(company);
+    function renderData(arrData, rowPerPage, page) {
+        page--;
+        cleanData();
+        sectionWrapper.innerHTML = `
+            <ul class="section__texts">
+                <li class="section__titles">Customer Name</li>
+                <li class="section__titles">Company</li>
+                <li class="section__titles">Phone Number</li>
+                <li class="section__titles">Email</li>
+                <li class="section__titles">Country</li>
+                <li class="section__titles">status</li>
+            </ul>`;
 
-    const phone = document.createElement(`li`);
-    phone.className = `section__texts__text`;
-    phone.innerText = userPhone;
-    phoneTable.append(phone);
+        const start = rowPerPage * page;
+        const end = start + rowPerPage;
 
-    const email = document.createElement(`li`);
-    email.className = `section__texts__text`;
-    email.innerText = userEmail;
-    emailTable.append(email);
+        let footerText = document.querySelector(`.footer__text`);
+        footerText.innerText = `Showing data ${start + 1} to ${
+           arrData.length > (currentPage * rowPerPage) 
+               ? (currentPage * rowPerPage) 
+               : arrData.length} of ${arrData.length} entries`
 
-    const country = document.createElement(`li`);
-    country.className = `section__texts__text`;
-    country.innerText = userCoutry;
-    countryTable.append(country);
+        const paginatedData = arrData.slice(start, end);
 
-    const statusContainer = document.createElement(`li`);
-    const activeStatus = document.createElement(`div`);
-    activeStatus.className = `section__status__active`;
-    activeStatus.innerText = `Active`;
-    const inactiveStatus = document.createElement(`div`);
-    inactiveStatus.className = `section__status__inactive`;
-    inactiveStatus.innerText = `Inactive`;
-    statusTable.append(statusContainer);
+        paginatedData.forEach((user) => renderUser(user))
 
-    if(userStatus === true) {
-        statusContainer.append(activeStatus);
+        function renderUser(user) {
+            const userList = document.createElement(`ul`);
+            userList.className = `section__texts`
+            sectionWrapper.append(userList);
+
+            const name = document.createElement(`li`);
+            name.className = `section__texts__text`;
+            name.innerText = user.name;
+            userList.append(name);
+
+            const company = document.createElement(`li`);
+            company.className = `section__texts__text`;
+            company.innerText = user.company;
+            userList.append(company);
+
+            const phone = document.createElement(`li`);
+            phone.className = `section__texts__text`;
+            phone.innerText = user.phone;
+            userList.append(phone);
+
+            const email = document.createElement(`li`);
+            email.className = `section__texts__text`;
+            email.innerText = user.email;
+            userList.append(email);
+
+            const country = document.createElement(`li`);
+            country.className = `section__texts__text`;
+            country.innerText = user.country;
+            userList.append(country);
+
+            if(user.status === true) {
+                const statusActive = document.createElement(`li`);
+                statusActive.className = `section__status__active`;
+                statusActive.innerText = `Active`;
+                userList.append(statusActive);
+            }
+            if(user.status !== true) {
+                const statusInactive = document.createElement(`li`);
+                statusInactive.className = `section__status__inactive`;
+                statusInactive.innerText = `Inactive`;
+                userList.append(statusInactive);
+            }
+        }
     }
-    else {
-        statusContainer.append(inactiveStatus);
+
+    function displayPagination(arrData, rowPerPage) {
+        const paginationEl = document.querySelector('.footer__wrapper');
+
+        const pagesCount = Math.ceil(arrData.length / rowPerPage);
+        const ulEl = document.createElement("ul");
+        ulEl.classList.add('footer__paginator');
+
+        for (let i = 0; i < pagesCount; i++) {
+            const liEl = displayPaginationBtn(i + 1);
+            ulEl.append(liEl)
+        }
+
+        paginationEl.append(ulEl)
+
+        function displayPaginationBtn(page) {
+            const liEl = document.createElement("li");
+            liEl.classList.add('footer__page')
+            liEl.innerText = page
+
+            if (currentPage === page) liEl.classList.add('footer__page__active');
+
+            liEl.addEventListener('click', () => {
+                currentPage = page
+                renderData(arrData, rows, currentPage)
+
+                let currentItemLi = document.querySelector('li.footer__page__active');
+                currentItemLi.classList.remove('footer__page__active');
+                liEl.classList.add('footer__page__active');
+            })
+            return liEl;
+        }
     }
+
+    function cleanPaginator() {
+        const ulEl = document.querySelectorAll(`.footer__paginator`);
+        ulEl.forEach((item) => item.remove())
+    }
+
+    function cleanData() {
+        sectionWrapper
+            .querySelectorAll(`.section__texts__text`)
+            .forEach(e => e.remove() );
+        sectionWrapper
+            .querySelectorAll(`.section__status__active`)
+            .forEach(e => e.remove() );
+        sectionWrapper
+            .querySelectorAll(`.section__status__inactive`)
+            .forEach(e => e.remove() );
+    }
+
+    cleanData();
+    renderData(data, rows, currentPage);
+    displayPagination(data, rows);
 }
-
-
 
 renderSection();
+
+
+
 
